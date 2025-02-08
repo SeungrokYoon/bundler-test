25be4394a65747cdf27243bb14d9670f5f85e831
SeungrokYoon <sonicrok93@gmail.com>
2025-02-09 00:43:35 +0900

    :sparkles: Feat :  http-server 설정을 통해 webpack 사용하지 않고 브라우저에 react app 구동시도-실패

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-// 기존 Body에 있는 내용을 지움
-document.body.innerHTML = '<div id="app"></div>';
-
 // 새로운 루트를 생성
-const root = createRoot(document.getElementById("app"));
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
