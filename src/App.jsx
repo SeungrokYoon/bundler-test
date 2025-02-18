72714e6b7298d7848df46f39d82b23c1450586f7
SeungrokYoon <sonicrok93@gmail.com>
2025-02-18 11:41:06 +0900

    🔧 Env: styled-components 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./style.css";
+import styled from "styled-components";
+
+const Button = styled.div`
+  background: papayawhip;
+  font-size: 2rem;
+  color: red;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  border: 1px solid black;
+`;
 
 export default function App() {
   return (
     <div>
       <h1 className="main-title">Hello, world</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
