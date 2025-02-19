d01092982d4aa989354eb1ee79609ca1c1e96d57
SeungrokYoon <sonicrok93@gmail.com>
2025-02-19 13:04:32 +0900

    🔧 Env: Prettier 설정 및 스크립트 추가

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * prettier.config.mjs
+ * @description prettier config 공식문서에서 제공하는 예시
+ */
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
