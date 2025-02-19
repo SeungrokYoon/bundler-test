a4168bddff3e6de8d999f16c44a2b412402ba1b3
SeungrokYoon <sonicrok93@gmail.com>
2025-02-19 13:05:18 +0900

    🔧 Env: gitIgnore파일에 포함된 파일들은 린트목록에서 제거, prettier와 통합

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 import pluginImport from "eslint-plugin-import";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: ["*.config.{js,mjs,cjs}"],
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  pluginImport.configs.recommended,
+  pluginImport.flatConfigs.recommended,
   {
+    files: ["src/**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
     rules: {
       "react/react-in-jsx-scope": "off",
       "import/order": [
@@ -57,4 +70,7 @@ export default [
       ],
     },
   },
+  eslintConfigPrettier,
+  // eslint.config.js (flat config) 에서는 config 순서에 따라 적용되는 순서가 변경된다.
+  // rules 밑에 eslintConfigPrettier가 위치하는 경우, eslintConfigPrettier가 rules를 override할 수 있다.
 ];
